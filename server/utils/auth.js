0da867b32114dc277f012ae21b5200f5c47fbed9
Danny <danny.f.gomez@gmail.com>
2024-06-22 15:57:52 -0400

    init commit

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,9 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const secret = 'mysecretssshhhhhhh';
-const expiration = '2h';
+const secret = process.env.SESSION_SECRET;
+const expiration = '48h';
 
 module.exports = {
   AuthenticationError: new GraphQLError('Could not authenticate user.', {
