d0d3abdc2656458719f6fb5d8706c9cb3642a18e
Danny <danny.f.gomez@gmail.com>
2024-06-22 15:55:27 -0400

    init commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# fitsync-pro
---
+++ server/utils/geminiAPI.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const prompt = "Create a workout plan for the week for a 30 year old user in moderate shape to increase strength by 2%. They can currenly do 50 pushups and 100 squats.";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
