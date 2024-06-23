70b2c3d96c35ccf13659713a9dee1b992b87a5d4
Danny <danny.f.gomez@gmail.com>
2024-06-22 20:39:43 -0400

    rename ai function and exported it

--- server/utils/gemini.js
+++ server/utils/gemini.js
@@ -3,10 +3,8 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-async function run() {
+async function generateAIresponse(prompt) {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-  const prompt = "Create a workout plan for the week for a 30 year old user in moderate shape to increase strength by 2%. They can currenly do 50 pushups and 100 squats.";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
@@ -14,4 +12,7 @@ async function run() {
   console.log(text);
 }
 
-run();
+const test = "Create a workout plan for the week for a 30 year old user in moderate shape to increase strength by 2%. They can currenly do 50 pushups and 100 squats.";
+generateAIresponse(test);
+
+export default run
