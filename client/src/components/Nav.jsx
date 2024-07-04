7a44037158436d1f53e384ca162bc136abde9b23
kaylommy <kaylajfreeman13@gmail.com>
2024-07-04 19:58:43 -0400

    added isLoggedIn Auth to the navbar to hide and show buttons if logged in or not

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { AppBar, Toolbar, Typography, Button, ThemeProvider, createTheme } from '@mui/material';
 import { useNavigate, useLocation } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 const theme = createTheme({
     palette: {
@@ -12,21 +13,29 @@ const theme = createTheme({
 function Nav() {
     const navigate = useNavigate();
     const location = useLocation();
+    const isLoggedIn = Auth.loggedIn();
 
-  return (
-    <ThemeProvider theme={theme}>
-    <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'white', cursor: 'pointer' }} onClick={() => navigate('/')}>
-          FitSync Pro
-        </Typography>
-        {location.pathname !== '/login' && (
-          <Button color="inherit" sx={{color: 'white' }} onClick={() => navigate('/login')}>Login</Button>
-        )}
-      </Toolbar>
-    </AppBar>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'white', cursor: 'pointer' }} onClick={() => navigate('/')}>
+                        FitSync Pro
+                    </Typography>
+                    {!isLoggedIn && location.pathname !== '/login' && (
+                        <Button color="inherit" sx={{color: 'white' }} onClick={() => navigate('/login')}>Login</Button>
+                    )}
+                    {isLoggedIn && (
+                        <>
+                            <Button color="inherit" sx={{color: 'white' }} onClick={() => navigate('/about-us')}>About Us</Button>
+                            <Button color="inherit" sx={{color: 'white' }} onClick={() => navigate('/profile')}>Profile</Button>
+                            <Button color="inherit" sx={{color: 'white' }} onClick={() => { Auth.logout(); navigate('/'); }}>Logout</Button>
+                        </>
+                    )}
+                </Toolbar>
+            </AppBar>
+        </ThemeProvider>
+    );
 }
 
 export default Nav;
