d8d5404a7eac456067994f2b6f61d6cf081fe45a
Danny <danny.f.gomez@gmail.com>
2024-07-04 23:23:56 -0400

    add auth to signup page to redirect if user already logged in

--- client/src/pages/signUpPage.jsx
+++ client/src/pages/signUpPage.jsx
@@ -1,15 +1,25 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SignUp from "../components/SignUp";
+import Auth from "../utils/auth"
 
+function SignUpPage() {
+    const navigate = useNavigate();
 
-function SignUpPage(){
-    return(
+    useEffect(() => {
+        const isLoggedIn = Auth.loggedIn();
+
+        if (isLoggedIn) {
+            navigate('/profile');
+        }
+    }, [navigate]);
+
+    return (
         <div>
-            <SignUp/>
-
+            <SignUp />
         </div>
-    )
+    );
 }
-
 
 
 export default SignUpPage;
