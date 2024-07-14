090a294d8aafea5225232ae2b1bb5e6732f053c8
kaylommy <kaylajfreeman13@gmail.com>
2024-07-14 00:34:47 -0400

    changed navbar and added get started to homepage as well as a hero

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react';
-import { AppBar, Toolbar, Typography, Button, ThemeProvider, createTheme, useMediaQuery, IconButton, Menu, MenuItem} from '@mui/material';
+import { useState, useEffect } from 'react';
+import { AppBar, Toolbar, Typography, Button, ThemeProvider, createTheme, useMediaQuery, IconButton, Menu, MenuItem, Box } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Auth from '../utils/auth';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SettingsIcon from '@mui/icons-material/Settings';
+
 const theme = createTheme({
     palette: {
         primary: {
@@ -17,15 +20,38 @@ function Nav() {
     const isLoggedIn = Auth.loggedIn(); // checks if user is logged in
     const matches = useMediaQuery(theme.breakpoints.down('sm')); // checks for screen size
     const [anchorEl, setAnchorEl] = useState(null);
+    const [isScrolled, setIsScrolled] = useState(false);
+
     const handleMenu = (event) => {
         setAnchorEl(event.currentTarget);
     }; // hamburger menu handler
+
     const handleClose = () => {
         setAnchorEl(null);
     }; // closing hamburger menu handler
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const position = window.pageYOffset;
+            setIsScrolled(position > 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        // Cleanup the event listener on component unmount
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
         <ThemeProvider theme={theme}>
-            <AppBar position="fixed">
+            <AppBar position="fixed" sx={{
+                backgroundColor: isScrolled ? 'rgba(147, 168, 126, 0.5)' : 'rgba(147, 168, 126, 0.5)',
+                backdropFilter: isScrolled ? 'blur(10px)' : 'none',
+                transition: 'background-color 0.3s ease, backdrop-filter 0.3s ease',
+                borderRadius: '30px',
+                marginTop: '1rem',
+                marginX: 'auto'
+            }}>
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'white', cursor: 'pointer', marginX: { sm: '0', md: '4rem' } }} onClick={() => navigate('/')}>
                         FitSync Pro
@@ -67,7 +93,7 @@ function Nav() {
                                         <MenuItem onClick={() => { handleClose(); navigate('/profile'); }}>Profile</MenuItem>
                                         <MenuItem onClick={() => { handleClose(); navigate('/settings'); }}>Settings</MenuItem>
                                         <MenuItem onClick={() => { Auth.logout(); handleClose(); navigate('/'); }}>Logout</MenuItem>
-                                        
+
                                     </>
                                 )}
                             </Menu>
@@ -79,13 +105,19 @@ function Nav() {
                             )}
                             {isLoggedIn && (
                                 <>
-                                    <Button sx={{ color: 'white', marginX: '2rem' }} onClick={() => navigate('/about-us')}>About Us</Button>
-                                    <Button sx={{ color: 'white', marginX: '2rem' }} onClick={() => navigate('/workout')}>Workout</Button>
-                                    <Button sx={{ color: 'white', marginX: '2rem' }} onClick={() => navigate('/fit-ai')}>Fit-AI</Button>
-                                    <Button sx={{ color: 'white', marginX: '2rem' }} onClick={() => navigate('/profile')}>Profile</Button>
-                                    <Button sx={{ color: 'white', marginX: '2rem' }} onClick={() => navigate('/settings')}>Settings</Button>
+                                    <Box sx={{ display: 'flex', justifyContent: 'center', flexGrow: 1, marginRight: '25rem' }}>
+                                        <Button sx={{ color: 'white', mx: '1rem' }} onClick={() => navigate('/about-us')}>About Us</Button>
+                                        <Button sx={{ color: 'white', mx: '1rem' }} onClick={() => navigate('/workout')}>Workout</Button>
+                                        <Button sx={{ color: 'white', mx: '1rem' }} onClick={() => navigate('/fit-ai')}>Fit-AI</Button>
+                                    </Box>
+                                    <IconButton sx={{ color: 'white', marginX: '2rem' }} onClick={() => navigate('/profile')}>
+                                        <AccountCircleIcon />
+                                    </IconButton>
+                                    <IconButton sx={{ color: 'white', marginX: '2rem' }} onClick={() => navigate('/settings')}>
+                                        <SettingsIcon />
+                                    </IconButton>
                                     <Button sx={{ color: 'white', marginX: '2rem' }} onClick={() => { Auth.logout(); navigate('/'); }}>Logout</Button>
-                                    
+
                                 </>
                             )}
                         </>
--- client/src/components/WelcomeComponent.jsx
+++ client/src/components/WelcomeComponent.jsx
@@ -1,16 +1,42 @@
 
-import { Grid, Paper, Typography, useMediaQuery } from '@mui/material';
+import { Grid, Paper, Typography, useMediaQuery, Button, Box } from '@mui/material';
 import fitnessImage from '../images/fitness-img-1.jpg';
+import heroImage from '../images/hero-image.jpg';
 
-function WelcomeComponent() {
+function WelcomeComponent({ scrollToGetStarted }) {
   const matches = useMediaQuery('(max-width:600px)');
   return (
     <Grid container spacing={0} justifyContent="center" alignItems="stretch" sx={{ 
       display: 'flex', 
       flexWrap: 'wrap', 
-      padding: '3rem',
-      paddingTop: matches ? '10rem' : '3rem',
        }}>
+          <Box sx={{ width: '100vw', padding: 0, margin: 0, height: '50vh' }}>
+        <Paper 
+          sx={{ 
+            position: 'relative', 
+            height: 600, 
+            backgroundImage: `url(${heroImage})`, 
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center', 
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center',
+            width: '100vw',
+            
+          }}
+        >
+          <Button variant="contained" color="primary" sx={{ 
+            position: 'absolute', 
+            bottom: 20, 
+            backgroundColor: '#46563c', 
+            '&:hover': {
+                backgroundColor: '#869f76' }, 
+                borderRadius: '30px' }}
+                onClick={scrollToGetStarted}>
+            Get Started
+          </Button>
+        </Paper>
+      </Box>
       <Grid item xs={12} sm={6} >
         <Paper elevation={0} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
           <Typography variant={matches ? "subtitle1" : "h6"} component="h3" padding={2} textAlign="center">
